39f0d74647a9c14630f69d8a8d56338d81a14694
OldWorldBoa <oldworldboa@gmail.com>
2026-04-07 12:56:27 +0000

    Update neovim-overlay.nix

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -83,17 +83,7 @@ with final.pkgs.lib; let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
-    {
-      'stevearc/oil.nvim',
-      ---@module 'oil'
-      ---@type oil.SetupOpts
-      opts = {},
-      -- Optional dependencies
-      dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-      -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-      -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-      lazy = false,
-    }
+    oil-nvim
   ];
 
   extraPackages = with pkgs; [
